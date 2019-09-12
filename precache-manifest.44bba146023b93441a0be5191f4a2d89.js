f2b0b602d0fd90cf82e303bcfcb5378d6f97375a
guigui64 <guillaume.comte10@gmail.com>
2019-09-13 00:36:18 +0200

    Updates

---
+++ precache-manifest.44bba146023b93441a0be5191f4a2d89.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2672a0d7368a1fb7c4bb53f6db0ca86",
+    "url": "/bookfinder/index.html"
+  },
+  {
+    "revision": "d1ca78e0730b744126a8",
+    "url": "/bookfinder/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "1d49b84add67b0c0aa52",
+    "url": "/bookfinder/static/css/main.44f884e8.chunk.css"
+  },
+  {
+    "revision": "d1ca78e0730b744126a8",
+    "url": "/bookfinder/static/js/2.d9db3bcf.chunk.js"
+  },
+  {
+    "revision": "1d49b84add67b0c0aa52",
+    "url": "/bookfinder/static/js/main.ff0a1f9e.chunk.js"
+  },
+  {
+    "revision": "7482eb302ad88ae7cb67",
+    "url": "/bookfinder/static/js/runtime~main.bcb46aae.js"
+  }
+]);
