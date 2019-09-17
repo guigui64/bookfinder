a4ca183fc39a27681d5aa3b129ab801b0ea2c889
guigui64 <guillaume.comte10@gmail.com>
2019-09-17 23:58:26 +0200

    Updates

---
+++ precache-manifest.b04f161119549cb00a15d4fad50743c6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c8ce3e84ea04235f8fca53d1f6606c5",
+    "url": "/bookfinder/index.html"
+  },
+  {
+    "revision": "1a048b2ae9057428da9b",
+    "url": "/bookfinder/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "896a3fab006a2d0fa02f",
+    "url": "/bookfinder/static/css/main.6bacca6f.chunk.css"
+  },
+  {
+    "revision": "1a048b2ae9057428da9b",
+    "url": "/bookfinder/static/js/2.7b32fe47.chunk.js"
+  },
+  {
+    "revision": "896a3fab006a2d0fa02f",
+    "url": "/bookfinder/static/js/main.9cfb1382.chunk.js"
+  },
+  {
+    "revision": "7482eb302ad88ae7cb67",
+    "url": "/bookfinder/static/js/runtime~main.bcb46aae.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/bookfinder/precache-manifest.0d3e5dd8cf3a230b57ac4694677fbf61.js"
+  "/bookfinder/precache-manifest.b04f161119549cb00a15d4fad50743c6.js"
 );
 
 self.addEventListener('message', (event) => {
