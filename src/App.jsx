a455a2b403ab5fc5c696f1f8520d4db3c2785f7c
guigui64 <guillaume.comte10@gmail.com>
2019-09-29 13:33:16 +0200

    footer + github corner

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,30 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
+import GithubCorner from 'react-github-corner';
 import Parent from './components/parent';
 
 function App() {
-	return <Parent />;
+	return (
+		<div id='container'>
+			<Parent />
+			<footer>
+				<div className='text-center'>
+					&copy; 2019 &ndash; Guillaume Comte &ndash; All rights reserved
+					&ndash; Find me on github{' '}
+					<a href='https://github.com/guigui64'>@guigui64</a> &ndash; Icons made
+					by{' '}
+					<a href='https://www.flaticon.com/authors/freepik' title='Freepik'>
+						Freepik
+					</a>{' '}
+					from{' '}
+					<a href='https://www.flaticon.com/' title='Flaticon'>
+						www.flaticon.com
+					</a>
+				</div>
+			</footer>
+			<GithubCorner href='https://github.com/guigui64/bookfinder' />
+		</div>
+	);
 }
 
 export default App;
--- src/components/parent.jsx
+++ src/components/parent.jsx
@@ -7,55 +7,39 @@ const Parent = () => {
 	const [books, setBooks] = useState([]);
 	const [noResult, setNoResult] = useState(false);
 	return (
-		<>
-			<div id='container'>
-				<div id='content'>
-					<nav className='text-center pt-3'>
-						<img src='logo.png' alt='logo' width='10%' />
-						<h2 className='text-uppercase mb-3'>Book Finder</h2>
-						<SearchBox setBooks={setBooks} setNoResult={setNoResult} />
-					</nav>
-					{/* TODO book cards */}
-					{noResult ? (
-						<p style={{ textAlign: 'center' }}>
-							Sorry, no result. Try another search.
-						</p>
-					) : (
-						<CardGroup>
-							{books.map((volume, i) => (
-								<BookCard
-									key={i}
-									imgSrc={
-										volume.volumeInfo.imageLinks &&
-										volume.volumeInfo.imageLinks.thumbnail
-									}
-									title={volume.volumeInfo.title}
-									author={
-										volume.volumeInfo.authors
-											? volume.volumeInfo.authors.join(', ')
-											: 'No author'
-									}
-									publisher={volume.volumeInfo.publisher}
-									link={volume.volumeInfo.infoLink}
-								/>
-							))}
-						</CardGroup>
-					)}
-				</div>
-				<footer>
-					<div className='text-center'>
-						Icons made by{' '}
-						<a href='https://www.flaticon.com/authors/freepik' title='Freepik'>
-							Freepik
-						</a>{' '}
-						from{' '}
-						<a href='https://www.flaticon.com/' title='Flaticon'>
-							www.flaticon.com
-						</a>
-					</div>
-				</footer>
-			</div>
-		</>
+		<div id='content'>
+			<nav className='text-center pt-3'>
+				<img src='logo.png' alt='logo' width='10%' />
+				<h2 className='text-uppercase mb-3'>Book Finder</h2>
+				<SearchBox setBooks={setBooks} setNoResult={setNoResult} />
+			</nav>
+			{/* TODO book cards */}
+			{noResult ? (
+				<p style={{ textAlign: 'center' }}>
+					Sorry, no result. Try another search.
+				</p>
+			) : (
+				<CardGroup>
+					{books.map((volume, i) => (
+						<BookCard
+							key={i}
+							imgSrc={
+								volume.volumeInfo.imageLinks &&
+								volume.volumeInfo.imageLinks.thumbnail
+							}
+							title={volume.volumeInfo.title}
+							author={
+								volume.volumeInfo.authors
+									? volume.volumeInfo.authors.join(', ')
+									: 'No author'
+							}
+							publisher={volume.volumeInfo.publisher}
+							link={volume.volumeInfo.infoLink}
+						/>
+					))}
+				</CardGroup>
+			)}
+		</div>
 	);
 };
 
